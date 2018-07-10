cce6f23b78835c185ef4d81a933dfba8454970d2
Esteban Hernandez <hernandez.dev.services@gmail.com>
2018-07-10 12:58:44 -0400

    Cleaned up spaces

--- src/color-picker/index.js
+++ src/color-picker/index.js
@@ -4,7 +4,8 @@ import { default as ColorPickerComponent } from './color-picker.component';
 
 import { default as ColorsService } from './colors.service';
 
-export const ColorPicker = () => {
-  const colorsService = new ColorsService();
-  return (<ColorPickerComponent colorsService={colorsService} />);
-}
+export const ColorPicker = () => (
+  <ColorPickerComponent
+    colorsService={new ColorsService()}
+  />
+);
